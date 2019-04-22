e885d1c10ef7787468893328422eba343ebcbf07
Angelika Tyborska <angelikatyborska@fastmail.com>
2019-04-22 16:00:05 +0200

    Implement advancing falling block

--- lib/falling_blocks/board.ex
+++ lib/falling_blocks/board.ex
@@ -1,5 +1,5 @@
 defmodule FallingBlocks.Board do
-  alias FallingBlocks.Block
+  alias FallingBlocks.{Block, Coordinates}
 
   @standard_width 10
   @standard_height 20
@@ -22,6 +22,45 @@ defmodule FallingBlocks.Board do
       Enum.any?(parts, fn part -> part == {x, y} end)
     end)
   end
+
+  @spec block_at?(Block.t() | nil, Coordinates.t()) :: boolean
+  def block_at?(nil, _), do: false
+
+  def block_at?(block, {x, y}) do
+    Enum.any?(block.parts, fn part -> part == {x, y} end)
+  end
+
+  @spec falling_block_at?(__MODULE__.t(), Coordinates.t()) :: boolean
+  def falling_block_at?(board, {x, y}) do
+    block_at?(board.falling_block, {x, y})
+  end
+
+  @spec advance(__MODULE__.t()) :: __MODULE__.t()
+  def advance(board) do
+    with %Block{} <- board.falling_block,
+         new_board <- %{board | falling_block: Block.advance(board.falling_block)},
+         false <- collisions?(new_board) do
+      new_board
+    else
+      nil ->
+        board
+
+      true ->
+        %{board | falling_block: nil, static_blocks: [board.falling_block | board.static_blocks]}
+    end
+  end
+
+  defp collisions?(board) do
+    with %Block{} <- board.falling_block do
+      board.falling_block.parts
+      |> Enum.any?(fn {_x, y} = part ->
+        find_static_block_at(board, part) ||
+          y >= board.height
+      end)
+    else
+      _ -> false
+    end
+  end
 end
 
 defimpl Inspect, for: FallingBlocks.Board do
@@ -30,15 +69,20 @@ defimpl Inspect, for: FallingBlocks.Board do
     |> Enum.map(fn row ->
       0..(board.width - 1)
       |> Enum.map(fn column ->
-        case FallingBlocks.Board.find_static_block_at(board, {column, row}) do
-          nil -> "."
-          %{type: :square} -> "*"
-          %{type: :long} -> "o"
-        end
+        block =
+          FallingBlocks.Board.find_static_block_at(board, {column, row}) ||
+            (FallingBlocks.Board.falling_block_at?(board, {column, row}) && board.falling_block) ||
+            nil
+
+        block_symbol(block)
       end)
       |> Enum.join(" ")
     end)
     |> Enum.join("\n")
     |> Kernel.<>("\n")
   end
+
+  defp block_symbol(%{type: :square}), do: "*"
+  defp block_symbol(%{type: :long}), do: "o"
+  defp block_symbol(_), do: "."
 end
--- test/falling_blocks/board_test.exs
+++ test/falling_blocks/board_test.exs
@@ -24,6 +24,125 @@ defmodule FallingBlocks.BoardTest do
     assert Board.find_static_block_at(board, {1, 1}) == nil
   end
 
+  describe "advance" do
+    test "it does nothing if no falling block" do
+      square1 = Block.square({0, 4})
+      square2 = Block.square({1, 2})
+
+      board = %Board{height: 6, width: 3, static_blocks: [square1, square2], falling_block: nil}
+
+      assert Board.advance(board) == board
+    end
+
+    test "it advances the falling block" do
+      square = Block.square({0, 0})
+      board = %Board{height: 6, width: 3, static_blocks: [], falling_block: square}
+
+      board2 = Board.advance(board)
+      board3 = Board.advance(board2)
+
+      assert inspect(board) == ~s"""
+             * * .
+             * * .
+             . . .
+             . . .
+             . . .
+             . . .
+             """
+
+      assert inspect(board2) == ~s"""
+             . . .
+             * * .
+             * * .
+             . . .
+             . . .
+             . . .
+             """
+
+      assert inspect(board3) == ~s"""
+             . . .
+             . . .
+             * * .
+             * * .
+             . . .
+             . . .
+             """
+    end
+
+    test "it stops the block when other block blocks it" do
+      square = Block.square({0, 2})
+      long = Block.long({0, 5})
+      board = %Board{height: 6, width: 4, static_blocks: [long], falling_block: square}
+
+      board2 = Board.advance(board)
+      board3 = Board.advance(board2)
+
+      assert inspect(board) == ~s"""
+             . . . .
+             . . . .
+             * * . .
+             * * . .
+             . . . .
+             o o o o
+             """
+
+      assert inspect(board2) == ~s"""
+             . . . .
+             . . . .
+             . . . .
+             * * . .
+             * * . .
+             o o o o
+             """
+
+      assert inspect(board3) == ~s"""
+             . . . .
+             . . . .
+             . . . .
+             * * . .
+             * * . .
+             o o o o
+             """
+    end
+
+    test "it stops the block when end of board" do
+      square = Block.square({0, 3})
+      board = %Board{height: 6, width: 4, static_blocks: [], falling_block: square}
+
+      board2 = Board.advance(board)
+      board3 = Board.advance(board2)
+
+      assert inspect(board) == ~s"""
+             . . . .
+             . . . .
+             . . . .
+             * * . .
+             * * . .
+             . . . .
+             """
+
+      assert inspect(board2) == ~s"""
+             . . . .
+             . . . .
+             . . . .
+             . . . .
+             * * . .
+             * * . .
+             """
+
+      assert inspect(board3) == ~s"""
+             . . . .
+             . . . .
+             . . . .
+             . . . .
+             * * . .
+             * * . .
+             """
+
+      assert board3.falling_block == nil
+    end
+  end
+
   describe "inspect" do
     test "empty board" do
       empty = %Board{height: 6, width: 3}
@@ -74,6 +193,22 @@ defmodule FallingBlocks.BoardTest do
              . * * .
              . * * .
              o o o o
+             """
+    end
+
+    test "a falling long" do
+      long = Block.long({0, 2})
+      board = %Board{height: 8, width: 4, static_blocks: [], falling_block: long}
+
+      assert inspect(board) == ~s"""
+             . . . .
+             . . . .
+             o o o o
+             . . . .
+             . . . .
+             . . . .
+             . . . .
+             . . . .
              """
     end
   end
