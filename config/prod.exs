5c18f7dbb707a57cab82865d685edc2db65c2c4e
Angelika Tyborska <angelikatyborska@fastmail.com>
2019-05-11 12:55:18 +0200

    Configure prod host

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :falling_blocks, FallingBlocksWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "localhost", port: {:system, "PORT"}], # This is critical for ensuring web-sockets properly authorize.
+  url: [host: "falling-tiles.angelika.me", port: {:system, "PORT"}], # This is critical for ensuring web-sockets properly authorize.
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   root: ".",
